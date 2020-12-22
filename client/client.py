eddad057bbe339eaa49772d3b2174a8c79e36f0e
hugofpaiva <hugofpaiva@ua.pt>
2020-12-22 19:41:28 +0000

    All comunications working and ciphered

--- client/client.py
+++ client/client.py
@@ -184,8 +184,9 @@ def main():
         digest = response['digest'].encode('latin')
         options = response['options'].encode('latin')
         iv = response['iv'].encode('latin')
+        print(options)
 
-        options = decryptor(selected_algorithm,selected_mode, MESSAGE_KEY, options, iv)
+        options, decryptor_var = decryptor(selected_algorithm,selected_mode, MESSAGE_KEY, options, iv)
 
         if verify_digest(DIGEST_KEY, selected_hash, options, digest):
             options = json.loads(options.decode('latin'))
@@ -236,52 +237,91 @@ def main():
     else:
         proc = subprocess.Popen(['ffplay', '-i', '-'], stdin=subprocess.PIPE)
 
+    decryptor_var = None
+
     # Get data from server and send it to the ffplay stdin through a pipe
-    for chunk in range(media_item['chunks'] + 1):
+    for chunk_id in range(media_item['chunks'] + 1):
         req = requests.get(
-            f'{SERVER_URL}/api/download?id={media_item["id"]}&chunk={chunk}')
+            f'{SERVER_URL}/api/download?id={media_item["id"]}&chunk={chunk_id}')
         chunk = req.json()
 
-        # TODO: Process chunk
+        if chunk_id == 0:
+            iv = chunk['iv'].encode('latin')
 
-        data = binascii.a2b_base64(chunk['data'].encode('latin'))
+        print("Chunk_Id - " + str(chunk_id) + "\n Chunk - "+ str(chunk))
+            
+        digest = chunk['digest'].encode('latin')
+
+        data = chunk['data'].encode('latin')
+
+        if decryptor_var:
+            data, decryptor_var = decryptor(selected_algorithm,selected_mode, MESSAGE_KEY, data, decryptor=decryptor_var)
+        else:
+            data, decryptor_var = decryptor(selected_algorithm,selected_mode, MESSAGE_KEY, data, iv)
+
+
+        if not verify_digest(DIGEST_KEY, selected_hash, data, digest):
+            print("MITM???")
+            quit()
+
+        data = binascii.a2b_base64(data)
+
         try:
+            
             proc.stdin.write(data)
         except:
             break
 
+    req = requests.get(f'{SERVER_URL}/api/logout')
+    if req.status_code == 200:
+        print("All done!")
+
 
 def encryptor(algorithm, mode, key, data, encryptor=None, block_size=None):
-        if encryptor:
-            if mode == 1:
-                padder = padding.PKCS7(block_size).padder()
-                padded_data = padder.update(data)
-                padded_data += padder.finalize()
-                ct = encryptor.update(data) + encryptor.finalize()
-            else:
-                ct = encryptor.update(data) + encryptor.finalize()
-            return ct, encryptor
+    if encryptor:
+        if mode == 1:
+            padder = padding.PKCS7(block_size).padder()
+            padded_data = padder.update(data)
+            padded_data += padder.finalize()
+            ct = encryptor.update(data) + encryptor.finalize()
         else:
-            # TODO Generate through secret module, also verify if all algorithms used are 128bits to generate only 16 bytes
-            iv = os.urandom(16)
-            algorithm = algorithms_options[algorithm](key)
-            cipher = Cipher(algorithm, cipher_modes_options[mode](iv))
-            encryptor = cipher.encryptor()
-            if mode == 1:
-                padder = padding.PKCS7(algorithms_options[algorithm].block_size*8).padder()
-                padded_data = padder.update(data)
-                padded_data += padder.finalize()
-                ct = encryptor.update(data) + encryptor.finalize()
-            else:
-                ct = encryptor.update(data) + encryptor.finalize()
-            return ct, encryptor, iv
+            ct = encryptor.update(data) + encryptor.finalize()
+        return ct, encryptor
+    else:
+        # TODO Generate through secret module, also verify if all algorithms used are 128bits to generate only 16 bytes
+        iv = os.urandom(algorithms_options[algorithm].block_size // 8)
+        cipher = Cipher(algorithms_options[algorithm](key), cipher_modes_options[mode](iv))
+        encryptor = cipher.encryptor()
+        if mode == 1:
+            padder = padding.PKCS7(algorithms_options[algorithm].block_size).padder()
+            padded_data = padder.update(data)
+            padded_data += padder.finalize()
+            ct = encryptor.update(padded_data) + encryptor.finalize()
+        else:
+            ct = encryptor.update(data) + encryptor.finalize()
+        return ct, encryptor, iv
 
-def decryptor(algorithm, mode, key, data, iv):
-    if algorithm == 0 or algorithm == 1:
-        algorithm = algorithms_options[algorithm](key)
-    cipher = Cipher(algorithm, cipher_modes_options[mode](iv))
-    decryptor = cipher.decryptor()
-    return decryptor.update(data)
+def decryptor(algorithm, mode, key, data, iv=None, decryptor = None, block_size=None, last = True):
+    if decryptor:
+        data = decryptor.update(data)
+        if mode == 1 and last:
+            if block_size is None:
+                block_size = algorithms_options[algorithm].block_size
+            unpadder = padding.PKCS7(block_size).unpadder()
+            data = unpadder.update(data)
+            data = data + unpadder.finalize()
+        return data, decryptor
+    else:
+        cipher = Cipher(algorithms_options[algorithm](key), cipher_modes_options[mode](iv))
+        decryptor = cipher.decryptor()
+        data = decryptor.update(data)
+        if mode == 1 and last:
+            if block_size is None:
+                block_size = algorithms_options[algorithm].block_size
+            unpadder = padding.PKCS7(block_size).unpadder()
+            data = unpadder.update(data)
+            data = data + unpadder.finalize()
+        return data, decryptor
 
 
 def digest(key, hash, data):
